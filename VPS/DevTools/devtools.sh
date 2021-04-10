c67d14154cc125649fb1956a0286c6b64ed18349
Daniel Alexis <danielalexis.o.pereira@gmail.com>
2021-04-10 17:25:24 +0100

    add necessary stuff

---
+++ VPS/DevTools/devtools.sh
@@ -0,0 +1,56 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root or sudo"
+  exit
+fi
+clear
+echo "-------------------------------"
+echo "This script is for Ubuntu 20.04"
+echo "-------------------------------"
+echo "     Updating Repositories     "
+sudo apt-get update > /dev/null
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed"
+    exit
+fi
+echo "-------------------------------"
+echo "        Installing Curl        "
+sudo apt-get install curl -y > /dev/null
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed"
+    exit
+fi
+echo "-------------------------------"
+echo "     Installing NodeJS 14.x    "
+echo "-------------------------------"
+echo "Adding the NodeSource repository"
+echo "-------------------------------"
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - > /dev/null
+echo "           Installing          "
+sudo apt-get install -y nodejs > /dev/null
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed"
+    exit
+fi
+echo "-------------------------------"
+echo "  Installing C/C++ (gcc & g++) "
+sudo apt-get install gcc g++ -y > /dev/null
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed"
+    exit
+fi
+echo "-------------------------------"
+echo "      Installing Python 3      "
+sudo apt-get install python3 python3-pip -y > /dev/null
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed"
+    exit
+fi
+echo "-------------------------------"
+echo "        Installing Perl        "
+sudo apt-get install perl -y > /dev/null
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed"
+    exit
+fi
+echo "-------------------------------"
+echo "Done"
